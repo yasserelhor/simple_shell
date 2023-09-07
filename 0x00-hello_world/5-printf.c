295030d19b8a3267fdb960579522dfa00bd5ce77
yasserelhor <elhoryasser6@gmail.com>
2023-09-07 06:14:28 -0700

    adding the "5-printf.c" script

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * The main function of the program
+ *
+ * This function is the entry point of the program.
+ * it prints a single message to the console.
+ *
+ * Return: 0 on successful execution.
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+	return (0);
+}
