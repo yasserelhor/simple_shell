a1981629ae260f38166d96994d07d4f345aa66bd
yasserelhor <elhoryasser6@gmail.com>
2023-09-08 05:06:46 -0700

    adding the "4-print_alphabt.c" program

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints
+ * the upper and lowercase alphabet characters from 'a'
+ * to 'z'.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alpha[] = "abcdfghijklmnoprstuvwxyz\n";
+	int i = 0;
+
+	while (alpha[i] != '\0')
+	{
+		putchar(alpha[i]);
+		i++;
+	}
+
+	return (0);
+}
