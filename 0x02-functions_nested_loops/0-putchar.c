dfa978779f63acd1318e2c0c65740dc5ccfadfa1
yasserelhor <elhoryasser6@gmail.com>
2023-09-12 04:05:30 -0700

    adding the 0-putchar.c program

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This is the main function for the program. It prints
+ *              "_putchar" to the standard output and returns 0 to indicate
+ *              successful execution.
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
