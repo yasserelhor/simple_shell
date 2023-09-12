e5efe47c0377798a8e85bd28c54800fe55559687
yasserelhor <elhoryasser6@gmail.com>
2023-09-12 09:56:08 -0700

    fixing the code

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - causes an infinite loop
+* Return: 0
+*/
+
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
