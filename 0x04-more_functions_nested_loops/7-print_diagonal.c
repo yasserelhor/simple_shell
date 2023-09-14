17bff0e0908a50c0c7ed2c6ceedd60f0deb2b24b
yasserelhor <elhoryasser6@gmail.com>
2023-09-14 06:47:44 -0700

    adding the thing

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Prints a diagonal line made of '\' characters.
+ * @n: The number of '\' characters to print.
+ *
+ * Description: This function prints a diagonal line consisting of '\' characters.
+ * If n is less than or equal to 0, it prints a newline.
+ */
+
+void print_diagonal(int n)
+{
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (postn = 1; postn <= n; postn++)
+		{
+			for (space = 1; space <= postn; space++)
+				_putchar(' ');
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+}
