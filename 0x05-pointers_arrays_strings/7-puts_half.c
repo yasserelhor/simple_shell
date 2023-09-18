36c671717e2036d85d20c9b147e7b840e3287d1d
yasserelhor <elhoryasser6@gmail.com>
2023-09-18 04:47:07 -0700

    adding the function

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts_half - Prints the second half of a string, followed by a newline.
+ *
+ * @str: Pointer to the input string.
+ */
+
+void puts_half(char *str)
+{
+	size_t length = strlen(str);
+
+	size_t div = length / 2;
+
+	int i = 0;
+
+	while (i != div)
+		printf(str[i]);
+		i++;
+	printf('\n');
+}
