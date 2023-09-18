dffb5667b32da7322205689a0e346ca3e50706a0
yasserelhor <elhoryasser6@gmail.com>
2023-09-18 04:34:01 -0700

    adding the function

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -19,10 +19,11 @@ void puts2(char *str)
 
 	while (str[nim] != '\0')
 	{
-		if (str[nim] != '\n')
-
-			printf("%c", str[nim]);
-			nim += 2;
+		if (i % 2 == 0)
+		{
+			_putchar(str[nim]);
+		}
+		nim++;
 	}
 	printf("\n");
 }
