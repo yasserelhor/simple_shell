d5c2a0048173abd2751a5c6b126172dd006923d8
yasserelhor <elhoryasser6@gmail.com>
2023-09-18 05:38:50 -0700

    adding the function

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -18,12 +18,10 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		if (a[i] != 98)
+		printf("%d", a[i]);
+		if (i < n - 1)
 		{
-			printf("%d, ", a[i]);
-		} else
-		{
-			printf("%d", a[i]);
+			printf(", ");
 		}
 	}
 	printf("\n");
