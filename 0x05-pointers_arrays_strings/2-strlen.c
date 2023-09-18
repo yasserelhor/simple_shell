f9be9f972c782a7acd8b1b2ca4c79be4f160dd68
yasserelhor <elhoryasser6@gmail.com>
2023-09-18 02:38:27 -0700

    adding the function

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - Calculate the length of a string.
+ *
+ * @s: The string to be measured.
+ *
+ * Return: The length of the string.
+ */
+
+int _strlen(char *s)
+{
+	int lenght = 0;
+
+	while (s[lenght] != '\0')
+		lenght++;
+
+	return (length);
+}
