55eb40b0725ece34bc0d2e7e712a13e7c8fe16cc
yasserelhor <elhoryasser6@gmail.com>
2023-09-18 06:34:59 -0700

    adding the function

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+/**
+ * main - generates random valid passwords for the program 101-crackme.
+ * Return: 0 is success
+ */
+
+int main(void)
+{
+	int remainingSum = 2772, index = 0, j, randomValue;
+	char password[100];
+	time_t currentTime;
+
+	srand((unsigned) time(&currentTime));
+	while (remainingSum > 0)
+	
+	{
+		randomValue = rand() % 94 + 33; 
+		if (randomValue <= remainingSum)
+		{
+			password[index] = randomValue;
+			remainingSum -= randomValue;
+			index++;
+		}
+	}	
+
+	for (j = 0; j <= i; j++)
+	{
+		printf("%c", password[j]);
+	}
+	return (0);
+}
