d738cc3bfa21d6a649095c6b56571f01dc2fc0cb
yasserelhor <elhoryasser6@gmail.com>
2023-09-21 08:08:52 -0700

    adding the function

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -11,12 +11,16 @@
 
 void print_number(int n)
 {
-	int i;
-
-	int len = strlen(n);
-
-	for (i = 0; i != n; i++)
+	if (n < 0)
 	{
-		_putchar(n[i])
+		_putchar('-');
+		n = -n;
 	}
+
+	if (n / 10 != 0)
+	{
+		print_number(n / 10);
+	}
+
+	print_digit(n % 10);
 }
