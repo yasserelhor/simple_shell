44ea8d0711c005eae488f407c2569c32f1d62d78
yasserelhor <elhoryasser6@gmail.com>
2023-09-28 09:10:20 -0700

    adding the function

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,28 @@
 #include "main.h"
 
 /**
+ * _actsq - Helper function to compute
+ * the square root of a non-negative integer.
+ * @n: The input integer.
+ * @i: The current value being tested.
+ *
+ * Return: The square root
+ * of n if it's a perfect square, -1 otherwise.
+ */
+
+int _actsq(int n, int i)
+{
+	if (n < 0)
+		return (-1);
+	if ((i * i) > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (_actsq(n, i + 1));
+}
+
+
+/**
  * _sqrt_recursion - Computes the square
  * root of a non-negative integer using recursion.
  * @n: The input integer.
@@ -10,26 +32,5 @@
 
 int _sqrt_recursion(int n)
 {
-	return (actsq(n, 1));
-}
-
-/**
- * actsq - Helper function to compute
- * the square root of a non-negative integer.
- * @n: The input integer.
- * @i: The current value being tested.
- *
- * Return: The square root
- * of n if it's a perfect square, -1 otherwise.
- */
-
-int actsq(int n, int i)
-{
-	if (n < 0)
-		return (-1);
-	if ((i * i) > n)
-		return (-1);
-	if (i * i == n)
-		return (i);
-	return (actsq(n, i + 1));
+	return (_actsq(n, 1));
 }
