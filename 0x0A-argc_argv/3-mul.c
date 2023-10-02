c16b22edd3a3e6a0612dc7376f9840c18e7a72d7
yasserelhor <elhoryasser6@gmail.com>
2023-10-02 05:49:06 -0700

    adding the program

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Multiplies two integers passed as command-line arguments.
+ * @argc: The number of command-line arguments.
+ * @argv: An array of strings containing the command-line arguments.
+ *
+ * Return: 0 on successful execution, 1 for errors.
+ */
+
+int main(int argc, char *argv[])
+{
+	int sum;
+
+	if ( argc == 3)
+	{
+		sum = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", sum);
+
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
