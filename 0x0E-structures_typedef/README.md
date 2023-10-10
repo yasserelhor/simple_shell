1cdd1e0704098cc86ce7334ff1142499b3c413d7
yasserelhor <elhoryasser6@gmail.com>
2023-10-10 07:11:54 -0700

    adding the thing

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+readme file is the best of all time :)
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,31 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+/**
+ * struct dog - struct that stores information about a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: This struct called "dog" stores information about a dog
+ *              including its name, age, and owner.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
