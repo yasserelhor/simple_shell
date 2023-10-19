902fb4abc5dc67e48fbb4f35f4ba7640f38f339e
yasserelhor <elhoryasser6@gmail.com>
2023-10-19 12:49:39 -0700

    adding the function

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,30 +1,22 @@
 #include "lists.h"
 
 /**
- * print_list - Prints the contents of a linked list
- * @h: A pointer to the head of the list
- *
- * This function iterates through
- * the linked list, printing each node's string
- * and its length, or "(nil)" if the string is NULL.
- *
- * Return: The number of nodes in the list.
+ * print_list - Print elements of a linked list.
+ * @h: A pointer to the head of the list.
+ * Return: Number of nodes in the list.
  */
 
 size_t print_list(const list_t *h)
 {
-	size_t c = 0;
+	size_t c;
 
+	c = 0;
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-		{
 			printf("[%d] %s\n", 0, "(nil)");
-		}
 		else
-		{
 			printf("[%d] %s\n", h->len, h->str);
-		}
 		h = h->next;
 		c++;
 	}
