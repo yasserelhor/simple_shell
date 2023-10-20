5680db9b29ee4e77ffb086ee378e48733abe8208
yasserelhor <elhoryasser6@gmail.com>
2023-10-20 12:19:54 -0700

    adding the function

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,22 +1,28 @@
 section .data
-    hello db 'Hello, Holberton',0
+    hello db 'Hello, Holberton',10
     hello_len equ $ - hello
 
 section .text
 global _start
 
 _start:
-    mov rax, 4
+    
+    mov rax, 1
 
+    
     mov rdi, 1
 
+    
     lea rsi, [hello]
 
+    
     mov rdx, hello_len
 
+   
     syscall
 
-    mov rax, 60       
+    
+    mov rax, 60
     xor rdi, rdi
     syscall
 
