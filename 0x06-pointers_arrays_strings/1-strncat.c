59352420b292d97a3423c4374aee83127ec703c5
yasserelhor <elhoryasser6@gmail.com>
2023-10-29 17:46:50 -0700

    adding the thing

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,13 +10,6 @@
  * Return: A pointer to the destination string (dest)
  */
 
-char *_strncat(char *dest, char *src, int n)
-{
-	int c, i;
-
-	c = 0;
-
-	while (dest[c])
 		c++;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
