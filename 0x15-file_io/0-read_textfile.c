d641f7dad0342bb09aa7adb7360075f14194078e
yasserelhor <elhoryasser6@gmail.com>
2023-10-30 13:37:39 -0700

    adding the thing

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * read_textfile - Reads and prints text from a file.
+ * @filename: The name of the file to read.
+ * @letters: The maximum number of letters to read and print.
+ * Return: The number of letters read and printed, or 0 on failure.
+ */
+
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	FILE *fn;
+	int c;
+	size_t i = 0;
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+
+	fn = fopen(filename, "r");
+	if (fn == NULL)
+	{
+		return (0);
+	}
+
+	while (i < letters && (c = fgetc(fn)) != EOF)
+	{
+		if (write(1, &c, 1) == -1)
+		{
+			fclose(fn);
+			return (0);
+		}
+		i++;
+	}
+
+	fclose(fn);
+	return (i);
+}
+
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,1 @@
+the best readme file in the world
